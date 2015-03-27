42f309d14beb5fdccba06cd221f618b1a75516f4
GFS <gfs@etherway.ru>
2015-03-27 17:00:38 +0300

    add angular to app.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -56,7 +56,7 @@
 	@yield('content')
 
 	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="/js/all.js"></script>
+	<script type="text/javascript" src="/js/vendor/angular.js"></script>
 </body>
 </html>
